f15e134f84b25c527b54d645e5d1869a2aa95914
markshawn2020 <shawninjuly@gmail.com>
2025-01-11 17:06:42 +0800

    feat(package.json): 添加 reflect-metadata、typedi 和 oh-my-commit 依赖

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,10 @@
+import 'reflect-metadata';
+import { Container, Service } from 'typedi';
+
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { VscodeLogger } from './vscode-logger';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -9,6 +13,25 @@ export function activate(context: vscode.ExtensionContext) {
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "oh-my-prompt" is now active!');
+
+
+
+
+@Service()
+class ExampleService {
+  constructor(
+    // because we annotated ExampleInjectedService with the @Service()
+    // decorator TypeDI will automatically inject an instance of
+    // ExampleInjectedService here when the ExampleService class is requested
+    // from TypeDI.
+    public logger: VscodeLogger
+  ) {
+  }
+}
+
+const serviceInstance = Container.get(ExampleService);
+serviceInstance.logger.info("hello");
+
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
---
+++ src/vscode-logger.ts
@@ -0,0 +1,47 @@
+/**
+ * @Copyright Copyright (c) 2024 Oh My Commit
+ * @CreatedAt 2024-12-30
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+import { Service } from "typedi";
+import vscode from "vscode";
+
+import {  BaseLogger, type ILogger, type LogLevel, formatMessage } from "@oh-my-commit/shared";
+
+const vscodeLogLevelMap: Record<vscode.LogLevel, LogLevel> = {
+  [vscode.LogLevel.Off]: "off",
+  [vscode.LogLevel.Trace]: "trace",
+  [vscode.LogLevel.Debug]: "debug",
+  [vscode.LogLevel.Info]: "info",
+  [vscode.LogLevel.Warning]: "warn",
+  [vscode.LogLevel.Error]: "error",
+};
+
+@Service()
+export class VscodeLogger extends BaseLogger implements ILogger {
+  override name = "host";
+
+  private logger = vscode.window.createOutputChannel("Oh My Prompt", {
+    log: true,
+  });
+
+  constructor() {
+    super();
+    this.logger.onDidChangeLogLevel((e: vscode.LogLevel) => {
+      this.setLevel(vscodeLogLevelMap[e]);
+    });
+    this.logger.show();
+  }
+
+  protected log(level: LogLevel, ...args: unknown[]) {
+    // todo: better handle with formatMessage
+    const rawMessage = formatMessage(...args);
+    const prefix = `omc.${this.name}`;
+    if (level === "off") {
+      return;
+    }
+    this.logger[level](`${prefix} ${rawMessage}`);
+  }
+}
