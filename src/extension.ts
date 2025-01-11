076915814fed7987b9919d9f80521975cd9690fb
markshawn2020 <shawninjuly@gmail.com>
2025-01-11 17:11:45 +0800

    refactor(extension): 重构扩展入口点

--- src/extension.ts
+++ src/extension.ts
@@ -1,21 +1,10 @@
-import 'reflect-metadata';
-import { Container, Service } from 'typedi';
+import "reflect-metadata";
+import { Container, Service } from "typedi";
 
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
-import { VscodeLogger } from './vscode-logger';
-
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "oh-my-prompt" is now active!');
-
-
-
+import * as vscode from "vscode";
+import { VscodeLogger } from "./vscode-logger";
 
 @Service()
 class ExampleService {
@@ -25,24 +14,32 @@ class ExampleService {
     // ExampleInjectedService here when the ExampleService class is requested
     // from TypeDI.
     public logger: VscodeLogger
-  ) {
-  }
+  ) {}
 }
 
-const serviceInstance = Container.get(ExampleService);
-serviceInstance.logger.info("hello");
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log('Congratulations, your extension "oh-my-prompt" is now active!');
 
+  const serviceInstance = Container.get(ExampleService);
+  serviceInstance.logger.info("hello");
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('oh-my-prompt.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Oh My Prompt!');
-	});
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  const disposable = vscode.commands.registerCommand(
+    "oh-my-prompt.helloWorld",
+    () => {
+      // The code you place here will be executed every time your command is executed
+      // Display a message box to the user
+      vscode.window.showInformationMessage("Hello World from Oh My Prompt!");
+    }
+  );
 
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,10 @@ const extensionConfig = {
   },
   resolve: {
     // support reading TypeScript and JavaScript files, 📖 -> https://github.com/TypeStrong/ts-loader
-    extensions: ['.ts', '.js']
+    extensions: ['.ts', '.js'],
+    alias: {
+      '@oh-my-commit/shared': path.resolve(__dirname, 'node_modules/@oh-my-commit/shared/dist/index.js')
+    }
   },
   module: {
     rules: [
