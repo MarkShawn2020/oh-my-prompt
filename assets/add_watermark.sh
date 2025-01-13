dd43ba1c07250d976f2f8d84f31f0ea37c8f24cc
markshawn2020 <shawninjuly@gmail.com>
2025-01-14 07:05:29 +0800

    feat: gif

--- assets/add_watermark.sh
+++ assets/add_watermark.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # 设置输入输出文件
-input_image="logo.jpeg"
-output_image="logo2.jpeg"
+input_image="demo.gif"
+output_image="demo2.gif"
 
 # 设置文字内容
 text1="Oh My Prompt"
@@ -21,9 +21,8 @@ image_size=$(magick identify -format "%wx%h" "$input_image")
 width=$(echo $image_size | cut -d'x' -f1)
 height=$(echo $image_size | cut -d'x' -f2)
 
-# 创建半透明橙红色背景的文字水印
-magick "$input_image" \
-  \( -size "${width}x${strip_height}" \
+# 创建水印图层
+magick \( -size "${width}x${strip_height}" \
      xc:"rgba(216,74,27,0.9)" \
      -fill white \
      -stroke none \
@@ -34,9 +33,40 @@ magick "$input_image" \
      -pointsize $font_size2 \
      -gravity south \
      -annotate +0+25 "$text2" \
-  \) \
-  -gravity center \
-  -composite \
-  "$output_image"
+  \) watermark.png
+
+# 为GIF的每一帧添加水印
+if [[ "$input_image" =~ \.gif$ ]]; then
+  magick "$input_image" \
+    -coalesce \
+    null: watermark.png \
+    -gravity center \
+    -compose over \
+    -layers composite \
+    -layers optimize \
+    "$output_image"
+else
+  magick "$input_image" \
+    \( -size "${width}x${strip_height}" \
+       xc:"rgba(216,74,27,0.9)" \
+       -fill white \
+       -stroke none \
+       -font "$font_type" \
+       -pointsize $font_size1 \
+       -gravity north \
+       -annotate +0+25 "$text1" \
+       -pointsize $font_size2 \
+       -gravity south \
+       -annotate +0+25 "$text2" \
+    \) \
+    -gravity center \
+    -composite \
+    "$output_image"
+fi
+
+# 清理临时文件
+if [ -f "watermark.png" ]; then
+  rm watermark.png
+fi
 
 echo "水印添加完成！输出文件: $output_image"
